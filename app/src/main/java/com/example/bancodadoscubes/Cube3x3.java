b8fa7542e90c9a1a21fa573d64f439f2dff5bbd8
Hmarcal <h236663@dac.unicamp.br>
2022-11-22 15:15:15 -0300

    Initial Commit!

---
+++ app/src/main/java/com/example/bancodadoscubes/Cube3x3.java
@@ -0,0 +1,23 @@
+package com.example.bancodadoscubes;
+
+public class Cube3x3 {
+    private long _id;
+    private float tempo;
+
+    public Cube3x3(long _id, Float tempo){
+        this._id = _id;
+        this.tempo = tempo;
+    }
+
+    public long get_id(){return _id; }
+    public void set_id(long _id){this._id = _id; }
+    public float getTempo() {return tempo; }
+    public void setTempo(float tempo) {this.tempo = tempo; }
+
+    @Override
+    public String toString(){
+        return "\n" + "Tempo :"+tempo+"\n";
+    }
+
+}
+
